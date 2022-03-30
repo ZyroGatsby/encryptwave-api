b221a1026f690e58b1ecd226de1d2f67409aae45
codec <zyro.auson@gmail.com>
2022-03-30 14:55:01 +0100

    permutation cipher backend :tada:

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+class Password(BaseModel):
+    password: str
+    key: str
+
+app = FastAPI()
+
+origins = [
+    "http://search-smartly-ma3u143xd-zyrogatsby.vercel.app",
+    "https://search-smartly-ma3u143xd-zyrogatsby.vercel.app",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def read_root():
+    return {"Hello from SearchSmartly"}
+
+@app.post("/encode")
+async def encode_password(password: Password):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    text = password.password.upper()
+    permutedKey = password.key.upper()
+    result = ""
+
+    for i in text:
+        if i in alphabet:
+            result += permutedKey[alphabet.find(i)]
+        else:
+            result += i
+
+    return result
